6c66510d8cd2698b59733c62445038c4ddcfd95c
KM_CM <send.nex@gmail.com>
2025-08-01 13:52:40 +0300

    Update __init__.py

--- KM_CM_Vector/__init__.py
+++ KM_CM_Vector/__init__.py
@@ -8,7 +8,7 @@ _T = _TypeVar( '_T' )
 
 class Vector( _Generic[ _T ] ):
     """
-A mathematical ``Vector``.
+A mathematical ``Vector`` that goes on way beyond math, allowing to use it with any type.
     """
 
     def __init__( self, *Axes: _T ) -> None:
